b9428184c9c4bcfadd443d00652034f22bf51176
Rp-Shamita <4nm19cs145@nmamit.in>
2022-05-19 12:58:02 +0530

    Added new algorithms

--- ML_Project.py
+++ ML_Project.py
@@ -87,7 +87,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
 
-# In[10]:
+# In[7]:
 
 
 from sklearn.preprocessing import StandardScaler
@@ -108,7 +108,7 @@ print("LR classification accuracy:", accuracy(Y_test, predictions))
 
 # # KNN classifier algorithm
 
-# In[11]:
+# In[8]:
 
 
 from collections import Counter
@@ -144,7 +144,7 @@ class KNN:
         return most_common[0][0]
 
 
-# In[12]:
+# In[9]:
 
 
 if __name__ == "__main__":
@@ -161,4 +161,267 @@ if __name__ == "__main__":
     clf.fit(X_train, Y_train)
     predictions = clf.predict(X_test)
     print("KNN classification accuracy", accuracy(Y_test, predictions))
+
+
+# # Decision Tree
+
+# In[10]:
+
+
+from collections import Counter
+
+import numpy as np
+
+
+def entropy(y):
+    hist = np.bincount(y)
+    ps = hist / len(y)
+    return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+
+class Node:
+    def __init__(
+        self, feature=None, threshold=None, left=None, right=None, *, value=None
+    ):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100, n_feats=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_feats = n_feats
+        self.root = None
+
+    def fit(self, X, y):
+        self.n_feats = X.shape[1] if not self.n_feats else min(self.n_feats, X.shape[1])
+        self.root = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+
+    def _grow_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        # stopping criteria
+        if (
+            depth >= self.max_depth
+            or n_labels == 1
+            or n_samples < self.min_samples_split
+        ):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_features, self.n_feats, replace=False)
+
+        # greedily select the best split according to information gain
+        best_feat, best_thresh = self._best_criteria(X, y, feat_idxs)
+
+        # grow the children that result from the split
+        left_idxs, right_idxs = self._split(X[:, best_feat], best_thresh)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+        return Node(best_feat, best_thresh, left, right)
+
+    def _best_criteria(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_thresh = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._information_gain(y, X_column, threshold)
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_thresh = threshold
+
+        return split_idx, split_thresh
+
+    def _information_gain(self, y, X_column, split_thresh):
+        # parent loss
+        parent_entropy = entropy(y)
+
+        # generate split
+        left_idxs, right_idxs = self._split(X_column, split_thresh)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+
+        # compute the weighted avg. of the loss for the children
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = entropy(y[left_idxs]), entropy(y[right_idxs])
+        child_entropy = (n_l / n) * e_l + (n_r / n) * e_r
+
+        # information gain is difference in loss before vs. after split
+        ig = parent_entropy - child_entropy
+        return ig
+
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
+        return left_idxs, right_idxs
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+
+
+# In[11]:
+
+
+def accuracy(y_true, y_pred):
+       accuracy = np.sum(y_true == y_pred) / len(y_true)
+       return accuracy
+   
+clf = DecisionTree(max_depth=10)
+clf.fit(X_train, Y_train)
+
+y_pred = clf.predict(X_test)
+acc = accuracy(Y_test, y_pred)
+
+print("Accuracy:", acc)
+
+
+# # Naive Bayes 
+
+# In[12]:
+
+
+class NaiveBayes:
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        # calculate mean, var, and prior for each class
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self._classes):
+            X_c = X[y == c]
+            self._mean[idx, :] = X_c.mean(axis=0)
+            self._var[idx, :] = X_c.var(axis=0)
+            self._priors[idx] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        posteriors = []
+
+        # calculate posterior probability for each class
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + posterior
+            posteriors.append(posterior)
+
+        # return class with highest posterior probability
+        return self._classes[np.argmax(posteriors)]
+
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(-((x - mean) ** 2) / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator
+
+
+# In[13]:
+
+
+def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+    
+nb = NaiveBayes()
+nb.fit(X_train, Y_train)
+predictions = nb.predict(X_test)
+
+print("Naive Bayes classification accuracy", accuracy(Y_test, predictions))
+
+
+# # Random Forest
+
+# In[15]:
+
+
+
+
+
+def bootstrap_sample(X, y):
+    n_samples = X.shape[0]
+    idxs = np.random.choice(n_samples, n_samples, replace=True)
+    return X[idxs], y[idxs]
+
+
+def most_common_label(y):
+    counter = Counter(y)
+    most_common = counter.most_common(1)[0][0]
+    return most_common
+
+
+class RandomForest:
+    def __init__(self, n_trees=10, min_samples_split=2, max_depth=100, n_feats=None):
+        self.n_trees = n_trees
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_feats = n_feats
+        self.trees = []
+
+    def fit(self, X, y):
+        self.trees = []
+        for _ in range(self.n_trees):
+            tree = DecisionTree(
+                min_samples_split=self.min_samples_split,
+                max_depth=self.max_depth,
+                n_feats=self.n_feats,
+            )
+            X_samp, y_samp = bootstrap_sample(X, y)
+            tree.fit(X_samp, y_samp)
+            self.trees.append(tree)
+
+    def predict(self, X):
+        tree_preds = np.array([tree.predict(X) for tree in self.trees])
+        tree_preds = np.swapaxes(tree_preds, 0, 1)
+        y_pred = [most_common_label(tree_pred) for tree_pred in tree_preds]
+        return np.array(y_pred)
+
+
+# In[17]:
+
+
+def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+clf = RandomForest(n_trees=3, max_depth=10)
+
+clf.fit(X_train, Y_train)
+y_pred = clf.predict(X_test)
+acc = accuracy(Y_test, y_pred)
+
+print("Accuracy:", acc)
 
